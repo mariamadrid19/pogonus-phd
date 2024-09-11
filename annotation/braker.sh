a6e63104bb1f64eefbf220f4cc1bbdf7c5103658
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-09-11 14:48:13 +0200

    Create braker.sh

---
+++ annotation/braker.sh
@@ -0,0 +1,14 @@
+#install braker
+
+mamba create -n braker anaconda::perl anaconda::biopython bioconda::perl-app-cpanminus bioconda::perl-file-spec bioconda::perl-hash-merge bioconda::perl-list-util bioconda::perl-module-load-conditional bioconda::perl-posix bioconda::perl-file-homedir bioconda::perl-parallel-forkmanager bioconda::perl-scalar-util-numeric bioconda::perl-yaml bioconda::perl-class-data-inheritable bioconda::perl-exception-class bioconda::perl-test-pod bioconda::perl-file-which bioconda::perl-mce bioconda::perl-threaded bioconda::perl-list-util bioconda::perl-math-utils bioconda::cdbtools eumetsat::perl-yaml-xs bioconda::perl-data-dumper
+
+mamba activate braker
+
+#GeneMark-ETP
+#AUGUSTUS
+#Pyhton3
+#BAMTOOLS
+#DIAMOND
+#STRINGTIE2
+#BEDTools
+#GffRead
