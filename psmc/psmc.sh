f5d9c0c0907e97176bab9ff87cc68bd40094c7b4
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-15 15:48:43 +0200

    Update psmc.sh

--- psmc/psmc.sh
+++ psmc/psmc.sh
@@ -1,5 +1,14 @@
-module load SAMtools/1.16.1-GCC-11.3.0
+#!/bin/bash -l 
+#SBATCH --cluster=genius 
+#SBATCH --job-name psmc 
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=24:00:00 
+#SBATCH -A lp_svbelleghem
 
+cd /scratch/leuven/357/vsc35707/psmc
+
+module load SAMtools/1.16.1-GCC-11.3.0
 module load BCFtools/1.15.1-GCC-11.3.0
 
 samtools mpileup -C50 -uf sorted_prim_dud.fasta GC136107.dudPrim.filtered.sorted.nd.bam | bcftools view -c - | vcfutils.pl vcf2fq -d 10 -D 100 | gzip > diploid.fq.gz
