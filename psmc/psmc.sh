b7515d6296428a4e3527968a2aa449ae8e34af28
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-15 16:35:03 +0200

    Update psmc.sh

--- psmc/psmc.sh
+++ psmc/psmc.sh
@@ -8,7 +8,12 @@
 
 cd /scratch/leuven/357/vsc35707/psmc
 
-module load SAMtools/1.16.1-GCC-11.3.0
 module load BCFtools/1.15.1-GCC-11.3.0
 
 bcftools mpileup -C50 -f sorted_prim_dud.fasta GC136107.dudPrim.filtered.sorted.nd.bam | bcftools call -c | vcfutils.pl vcf2fq -d 10 -D 100 | gzip > GC136107_prim_dud.fq.gz
+
+utils/fq2psmcfa -q20 GC136107_prim_dud.fq.gz > GC136107_prim_dud.psmcfa
+
+psmc -N25 -t12 -r5 -p "4+25*2+4+6" -o GC136107_prim_dud.psmc GC136107_prim_dud.psmcfa
+
+utils/psmc_plot.pl GC136107_prim_dud GC136107_prim_dud.psmc
