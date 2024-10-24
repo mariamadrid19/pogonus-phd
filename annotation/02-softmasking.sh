4240bee364ad90b2d281a84dfa50a8a27a2f7168
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-24 11:19:49 +0200

    Update 02-softmasking.sh

--- annotation/02-softmasking.sh
+++ annotation/02-softmasking.sh
@@ -7,8 +7,12 @@
 #SBATCH -o softmask.%j.out
 #SBATCH -A lp_svbelleghem
 
+
+#to correctly install and run EDTA, had to install EDTA from yml file using mamba (https://github.com/oushujun/EDTA)
+#and then had to download and make NINJA from source zip file (https://github.com/TravisWheelerLab/NINJA/releases/tag/0.98-cluster_only)
 conda activate EDTA
  
 BuildDatabase -name dudRM sorted_prim_dud.fasta
+#need to tell RepeatModeler where the folder that holds NINJA is located
 RepeatModeler -ninja_dir /data/leuven/357/vsc35707/NINJA-0.98-cluster_only/NINJA -database dudRM -pa 24 -LTRStruct >& run.out 
 RepeatMasker -lib dudRM-families.fa -xsmall -pa 24 sorted_prim_dud.fasta
