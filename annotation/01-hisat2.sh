da3aa25c88e3945d9912c4638498472f44dfe5bc
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-24 15:28:16 +0200

    Update 01-hisat2.sh

--- annotation/01-hisat2.sh
+++ annotation/01-hisat2.sh
@@ -11,17 +11,18 @@ conda activate isoseq
 
 isoseq cluster POG_larveIsoSeq.demux.bam POG_unpolished_reads.demux.bam --verbose
 
-samtools fasta POG_unpolished_reads.demux.bam POG_unpolished_reads.demux.fasta
+module load SAMtools/1.16.1-GCC-11.3.0
+
+samtools fasta POG_unpolished_reads.demux.hq.bam
 
 module load HISAT2/2.1.0-intel-2018a
-module load SAMtools/1.16.1-GCC-11.3.0
 
 hisat2-build sorted_prim_dud.fasta sorted_prim_dud
 
-hisat2 -f -x sorted_prim_dud -U POG_unpolished_reads.demux.fasta -S Dud_mapped_IsoSeq.sam
+hisat2 -f -x sorted_prim_dud -U POG_unpolished_reads.demux.hq.fasta -S POG_mapped_RNA_dud.sam
 
-samtools view -h -q 30 -b Dud_mapped_IsoSeq.sam > Dud_mapped_IsoSeq_filtered.bam
-samtools sort Dud_mapped_IsoSeq_filtered.bam > Dud_mapped_IsoSeq.filtered.bam
-samtools index Dud_mapped_IsoSeq.filtered.bam
+samtools view -h -q 30 -b POG_mapped_RNA_dud.sam > POG_mapped_RNA_dud.filtered.bam
+samtools sort POG_mapped_RNA_dud.filtered.bam > POG_mapped_RNA_dud.sorted.filtered.bam
+samtools index POG_mapped_RNA_dud.sorted.filtered.bam
 
-isoseq collapse --do-not-collapse-extra-5exons Dud_mapped_IsoSeq.filtered.bam collapsed_isoseq_dud_reads.gff
+isoseq collapse --do-not-collapse-extra-5exons POG_mapped_RNA_dud.sorted.filtered.bam collapsed_isoseq_dud_reads.gff
