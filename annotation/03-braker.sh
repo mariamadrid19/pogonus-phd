6b911a8243a3ed2ad86e9a5f405fcef5789fc528
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-24 15:50:42 +0200

    Create 03-braker.sh

---
+++ annotation/03-braker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name braker
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=12
+#SBATCH --time=48:00:00
+#SBATCH -o braker.%j.out
+#SBATCH -A lp_svbelleghem
+
+conda activate braker
+
+braker.pl --cores=12 --species=pogonus_mRNA --genome=sorted_prim_dud.soft_masked.fasta --bam=POG_mapped_RNA_dud.sorted.filtered.bam --softmasking --AUGUSTUS_CONFIG_PATH=/data/leuven/357/vsc35707/Augustus/config/
