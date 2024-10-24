1eb78f79be0d87b61bad02b036d4ab7dd1136b64
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-24 15:02:49 +0200

    Update 01-hisat2.sh

--- annotation/01-hisat2.sh
+++ annotation/01-hisat2.sh
@@ -11,12 +11,14 @@ conda activate isoseq
 
 isoseq cluster POG_larveIsoSeq.demux.bam POG_unpolished_reads.demux.bam --verbose
 
+samtools fasta POG_unpolished_reads.demux.bam POG_unpolished_reads.demux.fasta
+
 module load HISAT2/2.1.0-intel-2018a
 module load SAMtools/1.16.1-GCC-11.3.0
 
 hisat2-build sorted_prim_dud.fasta sorted_prim_dud
 
-hisat2 -f -x sorted_prim_dud -U POG_IsoSeq_HiFi_demux.fastq -S Dud_mapped_IsoSeq.sam
+hisat2 -f -x sorted_prim_dud -U POG_unpolished_reads.demux.fasta -S Dud_mapped_IsoSeq.sam
 
 samtools view -h -q 30 -b Dud_mapped_IsoSeq.sam > Dud_mapped_IsoSeq_filtered.bam
 samtools sort Dud_mapped_IsoSeq_filtered.bam > Dud_mapped_IsoSeq.filtered.bam
