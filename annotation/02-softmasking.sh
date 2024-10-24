2d393469ce851a570fd4e4da6b6f4d9c6719c591
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-24 10:07:37 +0200

    Create 02-softmasking.sh

---
+++ annotation/02-softmasking.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name softmask
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=48:00:00
+#SBATCH -o softmask.%j.out
+#SBATCH -A lp_svbelleghem
+
+conda activate EDTA2
+ 
+BuildDatabase -name dudRM sorted_prim_dud.fasta
+RepeatModeler -database dudRM -threads 24 -LTRStruct >& run.out
+RepeatMasker -lib dudRM-families.fa -xsmall -pa 24 sorted_prim_dud.fasta -gff
