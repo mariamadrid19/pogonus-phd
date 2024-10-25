55620f3490d22e7f143245b0d9106ae03cf6139c
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-25 09:42:53 +0200

    Update 03-braker.sh

--- annotation/03-braker.sh
+++ annotation/03-braker.sh
@@ -2,11 +2,11 @@
 #SBATCH --cluster=genius
 #SBATCH --job-name braker
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=12
+#SBATCH --ntasks-per-node=24
 #SBATCH --time=48:00:00
 #SBATCH -o braker.%j.out
 #SBATCH -A lp_svbelleghem
 
 conda activate braker
 
-braker.pl --cores=12 --species=P_chalceus --genome=sorted_prim_dud.soft_masked.fasta --bam=POG_mapped_RNA_dud.sorted.filtered.bam --softmasking --AUGUSTUS_CONFIG_PATH=/data/leuven/357/vsc35707/Augustus/config/
+braker.pl --cores=24 --species=P_chalceus --genome=sorted_prim_dud.soft_masked.fasta --bam=POG_mapped_RNA_dud.sorted.filtered.bam --softmasking --AUGUSTUS_CONFIG_PATH=/data/leuven/357/vsc35707/Augustus/config/
