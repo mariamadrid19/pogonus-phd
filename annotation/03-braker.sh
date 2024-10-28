08a81d9aba9ae75b17e82250fa4e5d74f189946a
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-28 18:32:32 +0100

    Update 03-braker.sh

--- annotation/03-braker.sh
+++ annotation/03-braker.sh
@@ -7,6 +7,8 @@
 #SBATCH -o braker.%j.out
 #SBATCH -A lp_svbelleghem
 
-conda activate braker
+#BRAKER and AUGUSTUS are both installed in the PWD (locally, on the SCRATCH)
+#BRAKER was installed with the make file
+#AUGUSTUS was installed using Docker (as job, requires a lot of memory)
 
-singularity exec -B ${PWD}:${PWD} $PWD/BRAKER/braker3.sif braker.pl --threads=24 --species=P_chalceus --genome=sorted_prim_dud.fasta.masked --bam=POG_mapped_RNA_dud.sorted.filtered.bam --softmasking --AUGUSTUS_CONFIG_PATH=/scratch/leuven/357/vsc35707/Augustus/config
+singularity exec -B ${PWD}:${PWD} $PWD/BRAKER/braker3.sif braker.pl --threads=24 --AUGUSTUS_CONFIG_PATH=$PWD/Augustus/config --species=P_chalceus --genome=sorted_prim_dud.fasta.masked --bam=POG_mapped_RNA_dud.sorted.filtered.bam --softmasking
