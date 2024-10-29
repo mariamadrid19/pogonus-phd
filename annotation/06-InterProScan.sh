bc13c70a24d272cbe6f48381650e82beeae653a9
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-29 13:42:09 +0100

    Update 06-InterProScan.sh

--- annotation/06-InterProScan.sh
+++ annotation/06-InterProScan.sh
@@ -23,4 +23,5 @@ cd /scratch/leuven/357/vsc35707/genome_annotation/braker/Augustus/interproscan
 
 module load Java/11.0.20
 
-interproscan.sh -i /scratch/leuven/357/vsc35707/genome_annotation/braker/Augustus/augustus.hints.aa
+#need to remove the * from the aa file
+interproscan.sh -i /scratch/leuven/357/vsc35707/genome_annotation/braker/Augustus/augustus.hints.aa --outfile augustus_protein_interpro
