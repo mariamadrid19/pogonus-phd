01924b7899a6fa2f60905c766041f53e3b2c1c06
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-29 11:13:12 +0100

    Update 04-blastp.sh

--- annotation/04-blastp.sh
+++ annotation/04-blastp.sh
@@ -8,5 +8,12 @@
 #SBATCH -A lp_svbelleghem
 
 module load BLAST+/2.13.0-gompi-2022a
+
+#all the NCBI BLAST databases: https://ftp.ncbi.nlm.nih.gov/blast/db/
+
+wget ftp://ftp.ncbi.nlm.nih.gov/blast/db/swissprot.tar.gz
+wget ftp://ftp.ncbi.nlm.nih.gov/blast/db/swissprot.tar.gz.md5
+
+tar -xvzf swissprot.tar.gz
   
 blastp -db swissprot -query ../augustus.hints.h.aa -outfmt 5 -max_target_seqs 10 -max_hsps 1 -out blastout.swiss.h.xml
