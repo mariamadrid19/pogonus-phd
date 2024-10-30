6cdc18b0d98b933acb0623611f22d4ef49e16fbe
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-30 10:08:01 +0100

    Update 05-parse_blastp.py

--- annotation/05-parse_blastp.py
+++ annotation/05-parse_blastp.py
@@ -3,7 +3,15 @@ from Bio.Blast import NCBIXML
 
 E_VALUE_THRESH = 0.04
 
-with open("blastout.swiss.h.xml") as blast_file, open("parsed_blastout.tsv", "w") as output_file:
+with open("augustus.blastout.swiss.xml") as blast_file, open("augustus_parsed_blastout.tsv", "w") as output_file:
+    for record in NCBIXML.parse(blast_file):
+        if record.alignments:
+            for j, align in enumerate(record.alignments):
+                for i, hsp in enumerate(align.hsps):
+                    if (j == 0) & (i == 0):
+                        output_file.write(f"{j}\t{i}\t{record.query}\t{hsp.expect}\t{align.title}\n")
+
+with open("braker.blastout.swiss.xml") as blast_file, open("braker_parsed_blastout.tsv", "w") as output_file:
     for record in NCBIXML.parse(blast_file):
         if record.alignments:
             for j, align in enumerate(record.alignments):
