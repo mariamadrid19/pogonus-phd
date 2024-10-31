7c113fd05c63cac9dde2fca501b728c90fcfb82d
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-31 15:05:21 +0100

    Create 00-db.sh

---
+++ decontamination/00-db.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name download_db
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=48:00:00
+#SBATCH -o download_db.%j.out
+#SBATCH -A lp_svbelleghem
+
+cd /scratch/leuven/357/vsc35707/blobtools/nt
+
+# Download files from nt.000.tar.gz to nt.192.tar.gz along with their .md5 files
+for i in $(seq -w 0 192); do
+    wget "https://ftp.ncbi.nlm.nih.gov/blast/db/nt.$i.tar.gz"
+    wget "https://ftp.ncbi.nlm.nih.gov/blast/db/nt.$i.tar.gz.md5"
+done
+
+# Verify the integrity of each .tar.gz file using its .md5 file
+for file in *.tar.gz; do
+    md5file="$file.md5"
+    # Extract the expected MD5 checksum from the .md5 file
+    expected_md5=$(cut -d ' ' -f 1 "$md5file")
+    # Calculate the MD5 checksum of the downloaded file
+    actual_md5=$(md5sum "$file" | awk '{print $1}')
+    
+    # Compare the checksums
+    if [[ "$expected_md5" == "$actual_md5" ]]; then
+        echo "$file: MD5 checksum matches"
+    else
+        echo "$file: MD5 checksum does NOT match"
+    fi
+done
+
+# Extract all downloaded .tar.gz files if they passed the MD5 check
+for file in *.tar.gz; do
+    tar -pxvzf "$file"
+done
