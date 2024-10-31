072ffec382e528a5bf5e5f7dae8110904cb7f0fe
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-10-31 11:33:58 +0100

    Update 03-braker.sh

--- annotation/03-braker.sh
+++ annotation/03-braker.sh
@@ -7,8 +7,9 @@
 #SBATCH -o braker.%j.out
 #SBATCH -A lp_svbelleghem
 
-#BRAKER and AUGUSTUS are both installed in the PWD (locally, on the SCRATCH)
+#BRAKER and AUGUSTUS are both installed on the VSC_DATA folder
 #BRAKER was installed with the singularity build
-#AUGUSTUS was installed using Docker (as job, requires a lot of memory)
+#AUGUSTUS was installed using Docker
+#Both were sent as jobs, installation requires a lot of memory 
 
 singularity exec -B ${PWD}:${PWD} $PWD/BRAKER/braker3.sif braker.pl --threads=24 --AUGUSTUS_CONFIG_PATH=$PWD/Augustus/config --species=P_chalceus --genome=sorted_prim_dud.fasta.masked --bam=POG_mapped_RNA_dud.sorted.filtered.bam --softmasking
