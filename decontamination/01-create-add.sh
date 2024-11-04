c7d3036065511740feac5b8225d8b96856298dd4
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-11-04 14:01:39 +0100

    Update and rename 01-create.sh to 01-create-add.sh

--- decontamination/01-create-add.sh
+++ decontamination/01-create-add.sh
@@ -18,11 +18,13 @@ blastn -db nt \
        -max_hsps 1 \
        -evalue 1e-25 \
        -num_threads 16 \
-       -out sorted_prim_dud.ncbi.blastn.out.txt"
+       -out sorted_prim_dud.ncbi.blastn.out.txt
        
 blobtools add \
     --hits sorted_prim_dud.ncbi.blastn.out.txt \
     --taxrule bestsumorder \
     --taxdump /scratch/leuven/357/vsc35707/blobtools/taxdump
+
+blobtools add --cov Dud_mapped_IsoSeq.filtered.bam --threads 24 --replace
 
 blobtools create -i sorted_prim_dud.fasta -b mapping_sorted.bam -t sorted_prim_dud.ncbi.blastn.out.txt -o filename
