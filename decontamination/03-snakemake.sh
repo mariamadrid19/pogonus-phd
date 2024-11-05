f4c80267dee3a69773b9507a3259b59c3b7ccdc6
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-11-05 10:03:28 +0100

    Create 03-snakemake.sh

---
+++ decontamination/03-snakemake.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name run_smk
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=72:00:00
+#SBATCH -o smk.%j.out
+#SBATCH -A lp_svbelleghem
+
+source /data/354/vsc35429/miniconda3/etc/profile.d/conda.sh
+
+conda activate btk_env
+
+# The working directory
+WORKDIR=/scratch/leuven/357/vsc35707/blobtools/sorted_prim_dud       
+# The assembly prefix
+ASSEMBLY=sorted_prim_dud
+# A directory to contain the Conda environments for individual Snakemake rules
+CONDA_DIR=/scratch/leuven/357/vsc35707/blobtools/blobtoolkit/.conda
+# The maximum number of parallel threads to run
+THREADS=64
+
+snakemake -p \
+          --use-conda \
+          --conda-prefix $CONDA_DIR \
+          --directory $WORKDIR/ \
+          --configfile $WORKDIR/config.yaml \
+          --stats $ASSEMBLY.snakemake.stats \
+          -j $THREADS \
+          --resources btk=1
