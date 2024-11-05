e527b1384f03a275daf3974ef27723e741e93d20
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-11-05 11:01:56 +0100

    Update 01-create-add.sh

--- decontamination/01-create-add.sh
+++ decontamination/01-create-add.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -l
 #SBATCH --cluster=genius
-#SBATCH --job-name download_db
+#SBATCH --job-name blobtools
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=24
 #SBATCH --time=48:00:00
-#SBATCH -o download_db.%j.out
+#SBATCH -o blobtools.%j.out
 #SBATCH -A lp_svbelleghem
 
 cd /scratch/leuven/357/vsc35707/blobtools/sorted_prim_dud/
 
 conda activate btk
 
-blobtools create --fasta sorted_prim_dud.fasta sorted_prim_dud
+blobtools create --fasta sorted_prim_dud.fasta /scratch/leuven/357/vsc35707/blobtools/sorted_prim_dud/
 
 module load BLAST+/2.13.0-gompi-2022a
 
@@ -28,14 +28,14 @@ blobtools add \
     --hits sorted_prim_dud.ncbi.blastn.out \
     --taxrule bestsumorder \
     --taxdump /scratch/leuven/357/vsc35707/blobtools/taxdump \
-    sorted_prim_dud
+    /scratch/leuven/357/vsc35707/blobtools/sorted_prim_dud/
 
 blobtools add \
     --cov Dud_mapped_IsoSeq.filtered.bam \
-    sorted_prim_dud
+    /scratch/leuven/357/vsc35707/blobtools/sorted_prim_dud/
 
 blobtools add \
     --busco sorted_prim_dud.busco.bacteria.full_summary.tsv \
     --busco sorted_prim_dud.busco.archaea.full_summary.tsv \
     --busco sorted_prim_dud.busco.fungi.full_summary.tsv \
-    sorted_prim_dud
+    /scratch/leuven/357/vsc35707/blobtools/sorted_prim_dud/
