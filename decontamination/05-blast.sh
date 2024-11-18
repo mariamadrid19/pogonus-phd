68497a9bcdc9857007cadc3328429f9b39e2c62e
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-11-18 14:23:29 +0100

    Create 05-blast.sh

---
+++ decontamination/05-blast.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name BLAST
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=48:00:00
+#SBATCH -o blast.%j.out
+#SBATCH -A lp_svbelleghem
+
+module load BLAST+/2.13.0-gompi-2022a
+
+export PATH=${HOME}/edirect:${PATH}
+
+sh get_species_taxids.sh -t 2 > bacterial.ids
+
+sh get_species_taxids.sh -t 4751 > fungal.ids
+
+sh get_species_taxids.sh -t 2157 > archaeal.ids
+
+blastn -db nt \
+       -query sorted_prim_dud.fasta \
+       -outfmt "6 qseqid staxids bitscore std" \
+       -max_target_seqs 1 \
+       -max_hsps 1 \
+       -evalue 1e-5 \
+       -num_threads 36 \
+       -taxidlist bacterial.ids, fungal.ids, archaeal.ids \
+       -out sorted_prim_dud.ncbi.blastn.out
+
+
