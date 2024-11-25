f489e783fcb82e6a5453f04dbca6abc9dfb01452
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-11-25 10:30:00 +0100

    Create 07-blast_chroms.sh

---
+++ decontamination/07-blast_chroms.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -l 
+#SBATCH --cluster=genius 
+#SBATCH --job-name blast_chroms
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=72:00:00
+#SBATCH --output=blast_chroms.%j.out
+#SBATCH -A lp_svbelleghem
+#SBATCH --array=1-16
+
+# Input files
+ASSEMBLY="sorted_prim_dud.fasta"
+SCAFFOLDS="chrom_names.txt"
+
+module load BLAST+/2.13.0-gompi-2022a
+
+export BLASTDB=/scratch/leuven/357/vsc35707/blobtools/sorted_prim_dud/nt
+
+SCAF=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$SCAFFOLDS")
+
+# Extract the scaffold sequence into a FASTA file
+awk -v scaffold=">${SCAF}" '
+BEGIN {print_seq=0}
+$0 ~ scaffold {print_seq=1; print $0; next}
+$0 ~ /^>/ {print_seq=0}
+print_seq' "$FASTA" > "${SCAF}.fasta"
+
+# Run BLAST for this scaffold
+blastn -db nt \
+       -query "${SCAF}.fasta" \
+       -outfmt "6 qseqid staxids bitscore std" \
+       -max_target_seqs 1 \
+       -max_hsps 1 \
+       -evalue 1e-25 \
+       -num_threads 32 \
+       -taxidlist microbial.ids \
+       -out "${SCAF}.ncbi.blastn.out"
+
+# Clean up scaffold FASTA file
+rm "${SCAF}.fasta"
