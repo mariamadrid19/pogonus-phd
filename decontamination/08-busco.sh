826f25c4ec73d0aea4e68486f284626e512ae7c1
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2024-12-16 12:49:34 +0100

    Update 08-busco.sh

--- decontamination/08-busco.sh
+++ decontamination/08-busco.sh
@@ -19,3 +19,8 @@ compleasm.py download fungi
 compleasm.py run -a sorted_prim_dud.fasta -o archaea/ -l archaea -t 24
 compleasm.py run -a sorted_prim_dud.fasta -o fungi/ -l fungi -t 24
 compleasm.py run -a sorted_prim_dud.fasta -o bacteria/ -l bacteria -t 24
+
+
+busco -i sorted_prim_dud.fasta -m genome -l archaea -c 24
+busco -i sorted_prim_dud.fasta -m genome -l fungi -c 24
+busco -i sorted_prim_dud.fasta -m genome -l bacteria -c 24
