aceaba78ec66931e986c0f952517159ff59d7783
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-01-23 19:22:00 +0100

    Update minigraph.sh

--- pangenomics/minigraph.sh
+++ pangenomics/minigraph.sh
@@ -1,3 +1,7 @@
+
+git clone https://github.com/lh3/minigraph
+cd minigraph && make
+
 #Build pangenome using minigraph
 minigraph -cxggs -L 100 -o pogonus.gfa sorted_prim_dud.fasta sorted_prim_nieu.fasta
 
