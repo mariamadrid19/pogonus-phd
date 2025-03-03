dbe9a95736141345c8e86a5d9315e7defe721ce5
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-03 17:19:42 +0100

    Update and rename qc.sh to 02_qc.sh

--- gwas/02_qc.sh
+++ gwas/02_qc.sh
@@ -9,12 +9,29 @@
 
 cd /scratch/leuven/357/vsc35707/GWAS/bams
 
-for bam in *.bam; do 
+module load SAMtools/1.9-GCC-6.4.0-2.28
+
+for bam in *.filtered.bam; do 
     echo "$bam"; 
     samtools flagstat "$bam"; 
 done > mapping_stats.txt
 
-for bam in *.bam; do 
+for bam in *.filtered.bam; do 
     echo -n "$(basename "$bam" .bam): "; 
     samtools depth -a "$bam" | awk '{sum+=$3} END {if (NR>0) print sum/NR; else print "No coverage"}'; 
 done > average_depths.txt
+
+# Output file for errors
+ERROR_LOG="bam_quickcheck_errors.txt"
+
+# Empty the error log file before running
+> "$ERROR_LOG"
+
+# Loop through all filtered.sorted.bam files
+for bam in *.filtered.sorted.bam; do
+    if ! samtools quickcheck "$bam"; then
+        echo "$bam" >> "$ERROR_LOG"
+    fi
+done
+
+echo "Check complete. Files with errors are listed in: $ERROR_LOG"
