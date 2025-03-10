2045724c84f6e9d2250b4621c7ca6305e6f04857
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-10 15:48:13 +0100

    Update 07_gemma.sh

--- gwas/07_gemma.sh
+++ gwas/07_gemma.sh
@@ -12,3 +12,13 @@ plink --vcf gwas_imputed.vcf.gz --pheno phenotype.txt --make-bed --out gwas_inpu
 
 # confirm that PLINK correctly read the phenotype file
 plink --bfile gwas_input --pheno phenotype.txt --assoc --out check_pheno
+
+
+source /data/leuven/357/vsc35707/miniconda3/etc/profile.d/conda.sh
+conda activate gemma
+
+# Generate a kinship matrix 
+gemma -bfile gwas_input -gk 1 -o kinship
+
+# Perform the GWAS with phenotype file and kinship matrix
+gemma -bfile gwas_input -k output/kinship.cXX.txt -lmm 4 -o gwas_results
