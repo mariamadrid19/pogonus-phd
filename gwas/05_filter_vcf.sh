d4ebac88d4fba5081ce26bff70f9dab88876f4d0
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-10 11:40:24 +0100

    Update 05_filter_vcf.sh

--- gwas/05_filter_vcf.sh
+++ gwas/05_filter_vcf.sh
@@ -7,9 +7,8 @@
 #SBATCH -A lp_svbelleghem
 #SBATCH -o filter_vcf.%j.out
 
-module load Python/3.7.0-foss-2018a
 module load VCFtools/0.1.16-GCC-10.3.0
-export BCFTOOLS_PLUGINS=/data/leuven/357/vsc35707/bcftools/plugins
+module load tabix/0.2.6-GCCcore-6.4.0
 
 # filter sites by call rate (missing data)
 vcftools --vcf merged_variants.vcf --max-missing 0.9 --recode --stdout | bgzip > filtered_merged_variants.vcf.gz
@@ -24,5 +23,4 @@ vcftools --vcf quality_filtered_merged_variants.vcf.gz --remove-indels --recode 
 vcftools --vcf snps_only.vcf.gz --maf 0.05 --recode --stdout | bgzip > gwas_filtered.vcf.gz
 
 # Index VCF file
-module load tabix/0.2.6-GCCcore-6.4.0
 tabix -p vcf gwas_filtered.vcf.gz
