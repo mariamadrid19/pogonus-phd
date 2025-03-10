6d4b831e18f1aba429d370cee5508cd867f93edb
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-10 15:42:05 +0100

    Update 07_gemma.sh

--- gwas/07_gemma.sh
+++ gwas/07_gemma.sh
@@ -9,3 +9,6 @@
 
 module load PLINK/1.9
 plink --vcf gwas_imputed.vcf.gz --pheno phenotype.txt --make-bed --out gwas_input
+
+# confirm that PLINK correctly read the phenotype file
+plink --bfile gwas_imputed_with_pheno --pheno phenotype.txt --assoc --out check_pheno
