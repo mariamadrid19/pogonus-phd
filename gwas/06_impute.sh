3a2b9db3a3b72bee50b3e63fe417dec9eea5a1d6
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-10 13:39:52 +0100

    Update 06_impute.sh

--- gwas/06_impute.sh
+++ gwas/06_impute.sh
@@ -8,3 +8,10 @@
 #SBATCH -o impute.%j.out
 
 # Beagle 5.5
+zcat gwas_filtered.vcf.gz | cut -f1-190 | tr '/' '|' | gzip > beagle_gwas_filtered.vcf.gz
+zcat beagle_gwas_filtered.vcf.gz | cut -f1-9,191-200 | gzip > beagle_gwas_filtered.vcf.gz
+
+echo
+echo "*** Running test analysis with \"gt=\" argument ***"
+echo
+java –Xmx20g  -jar beagle.27Feb25.75f.jar gt=beagle_gwas_filtered.vcf.gz out=beagle_gwas.gt
