87237f3a26e996b4712f20c6f6a092dd1cc34c5b
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-10 11:41:49 +0100

    Update and rename 06_gemma.sh to 07_gemma.sh

--- gwas/06_gemma.sh
+++
@@ -1,2 +0,0 @@
-module load PLINK/1.9
-plink --vcf final_filtered.vcf.gz --make-bed --out GWAS_input
---
+++ gwas/07_gemma.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -l 
+#SBATCH --cluster=genius 
+#SBATCH --job-name gwas 
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=20
+#SBATCH --time=72:00:00 
+#SBATCH -A lp_svbelleghem
+#SBATCH -o gwas.%j.out
+
+module load PLINK/1.9
+plink --vcf gwas_filtered.vcf.gz --make-bed --out GWAS_input
