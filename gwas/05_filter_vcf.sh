70d88352443489b584b7fe0153b526c2861ae760
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-10 15:36:36 +0100

    Update 05_filter_vcf.sh

--- gwas/05_filter_vcf.sh
+++ gwas/05_filter_vcf.sh
@@ -11,17 +11,23 @@ source /data/leuven/357/vsc35707/miniconda3/etc/profile.d/conda.sh
 conda activate vcftools
 module load tabix/0.2.6-GCCcore-6.4.0
 
-# filter sites by call rate (missing data)
-vcftools --vcf merged_variants.vcf --max-missing 0.9 --recode --stdout | bgzip > filtered_merged_variants.vcf.gz
+# Check missingness per individual
+vcftools --gzvcf merged_variants.vcf.gz --missing-indv
+
+# Filter out samples with more than 10% missing genotypes (keep those with ≥90% call rate)
+awk '$5 <= 0.1 {print $1}' out.imiss > high_call_samples.txt
+
+# Use --keep to retain only these samples
+vcftools --gzvcf merged_variants.vcf.gz --keep high_call_samples.txt --recode --stdout | bgzip > filtered_merged_variants.vcf.gz
 
 # filter sites by quality scores
-vcftools --vcf filtered_merged_variants.vcf.gz --minGQ 20 --recode --stdout | bgzip > quality_filtered_merged_variants.vcf.gz
+vcftools --gzvcf filtered_merged_variants.vcf.gz --minGQ 20 --recode --stdout | bgzip > quality_filtered_merged_variants.vcf.gz
 
 # remove indels, keep only snps
-vcftools --vcf quality_filtered_merged_variants.vcf.gz --remove-indels --recode --stdout | bgzip > snps_only.vcf.gz
+vcftools --gzvcf quality_filtered_merged_variants.vcf.gz --remove-indels --recode --stdout | bgzip > snps_only.vcf.gz
 
 # MAF filtering
-vcftools --vcf snps_only.vcf.gz --maf 0.05 --recode --stdout | bgzip > gwas_filtered.vcf.gz
+vcftools --gzvcf snps_only.vcf.gz --maf 0.05 --recode --stdout | bgzip > gwas_filtered.vcf.gz
 
 # Index VCF file
 tabix -p vcf gwas_filtered.vcf.gz
