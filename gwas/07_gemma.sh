c9daf5842098b6f7a6dc5dd986f70b1359eb3b54
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-10 16:25:09 +0100

    Update 07_gemma.sh

--- gwas/07_gemma.sh
+++ gwas/07_gemma.sh
@@ -7,12 +7,18 @@
 #SBATCH -A lp_svbelleghem
 #SBATCH -o gwas.%j.out
 
+module load Python/3.7.0-foss-2018a
+export BCFTOOLS_PLUGINS=/data/leuven/357/vsc35707/bcftools/plugins
+
+# Remove the samples that don't have a wing measurement (not found in the phenotype.txt file) 
+bcftools view --threads 20 --samples-file ^remove_samples.txt gwas_imputed.vcf.gz -Oz -o gwas_imputed_clean.vcf.gz
+bcftools index gwas_filtered_clean.vcf.gz
+
 module load PLINK/1.9
-plink --vcf gwas_imputed.vcf.gz --pheno phenotype.txt --make-bed --out gwas_input
+plink --vcf gwas_imputed_clean.vcf.gz --pheno phenotype.txt --make-bed --out gwas_input
 
 # confirm that PLINK correctly read the phenotype file
 plink --bfile gwas_input --pheno phenotype.txt --assoc --out check_pheno
-
 
 source /data/leuven/357/vsc35707/miniconda3/etc/profile.d/conda.sh
 conda activate gemma
