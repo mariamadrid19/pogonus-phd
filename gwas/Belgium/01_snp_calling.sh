c045eed2666bf2ce3a2f492569a68d8a5e0df92a
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-12 10:47:17 +0100

    Update 01_snp_calling.sh

--- gwas/Belgium/01_snp_calling.sh
+++ gwas/Belgium/01_snp_calling.sh
@@ -30,3 +30,39 @@ Db_101 Db_102 Db_103 \
 Pc_DZ_001 Pc_DZ_008 Pc_DZ_012 Pc_DZ_016 Pc_DZ_018 Pc_DZ_022 Pc_DZ_024 Pc_DZ_027 Pc_DZ_030 \
 GC129427 GC129428 GC129429 GC129430 GC129431 GC129432 GC129433 GC129434 GC129435 GC129437 GC129438 \
 GC136117 GC136119 GC136120 GC136121 GC136122 GC136123 GC136124 GC136125 GC136126 GC136127 GC136128)
+
+# Some folder and file paths to use later
+REF=/scratch/leuven/357/vsc35707/GWAS/sorted_prim_dud.fasta
+REFNAME=primDud
+POPULATION=Belgium
+
+cd /scratch/leuven/357/vsc35707/GWAS/$POPULATION
+# All the bam files should be here
+
+# make a single list of all the samples that can be used in the samtools command
+ALL_LIST=""
+for FILE in ${samples[*]}
+do
+ALL_LIST="$ALL_LIST $FILE".$REFNAME.filtered.sorted.nd.bam""
+done
+eval command=\$$(echo ALL_LIST)
+
+echo "Reference: $REF"
+echo "Chromosome: ${chrom[ID]}"
+echo "Command: $command"
+
+# Ensure chromosome ID is not empty
+if [[ -z "${chrom[ID]}" ]]; then
+    echo "Error: chrom[ID] is empty!"
+    exit 1
+fi
+
+# Ensure BAM files exist
+if [[ -z "$command" ]]; then
+    echo "Error: No BAM files specified!"
+    exit 1
+fi
+
+# run mpileup
+bcftools mpileup -Oz --threads 20 -f $REF $(echo $command) -r $(echo "${chrom[ID]}") | \
+bcftools call -m -Oz -o /scratch/leuven/357/vsc35707/GWAS/Pogonus_$POPULATION_$REFNAME.chr_$(echo "${names[ID]}").vcf.gz
