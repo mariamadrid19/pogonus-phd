bd1d899b3bf4679ed5c82be5ee97222889d657a1
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-12 11:31:02 +0100

    Update 03_splitting_imputing.sh

--- gwas/Belgium/03_splitting_imputing.sh
+++ gwas/Belgium/03_splitting_imputing.sh
@@ -29,7 +29,7 @@ echo
 
 # Java is needed to run BEAGLE (load the appropriate module)
 module load Java/11.0.20
-java -Xmx20480m -jar beagle.27Feb25.75f.jar gt=split_variants_$POPULATION.vcf.gz out=gwas_imputed_$POPULATION
+java -Xmx20480m -jar /data/leuven/357/vsc35707/beagle.27Feb25.75f.jar gt=split_variants_$POPULATION.vcf.gz out=gwas_imputed_$POPULATION
 
 # Index the imputed vcf file 
 bcftools index -t gwas_imputed_$POPULATION.vcf.gz
