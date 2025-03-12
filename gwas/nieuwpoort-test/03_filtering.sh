98454be30bedfbd7ad4b848de59d88a9e1c8ba01
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-12 09:21:54 +0100

    Update 03_filtering.sh

--- gwas/nieuwpoort-test/03_filtering.sh
+++ gwas/nieuwpoort-test/03_filtering.sh
@@ -14,11 +14,11 @@ module load tabix/0.2.6-GCCcore-6.4.0
 # Check missingness per individual
 vcftools --gzvcf merged_Npt.vcf.gz --missing-indv
 
-vcftools --gzvcf merged_Npt.vcf.gz --max-missing 0.9 --minQ 30 --maf 0.05 --remove-indels --recode --stdout | bgzip > gwas_filtered.vcf.gz
+vcftools --gzvcf merged_Npt.vcf.gz --max-missing 0.9 --minQ 30 --maf 0.05 --remove-indels --recode --stdout | bgzip > gwas_filtered_Npt.vcf.gz
 # At least 90% of individuals must have a called genotype
 # Minimum quality score of 30
 # Minor allele frequency (MAF) threshold of 0.05
 # Exclude indels, keeping only SNPs
 
 # Index VCF file
-tabix -p vcf gwas_filtered.vcf.gz
+tabix -p vcf gwas_filtered_Npt.vcf.gz
