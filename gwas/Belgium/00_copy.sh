b5f8c14813b525c05dd50ea0cdc6b2a1b5241e9b
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-13 10:38:13 +0100

    Update 00_copy.sh

--- gwas/Belgium/00_copy.sh
+++ gwas/Belgium/00_copy.sh
@@ -39,21 +39,25 @@ echo "File transfer complete."
 
 cd /scratch/leuven/357/vsc35707/GWAS/$POPULATION
 
-bcftools view -s GC129388,GC129389,GC129390,GC129391,GC129392,GC129393,Na_034,PcNP_034,PcNP_037,PcNP_040,PcNP_044,\
-PcNP_045,PcNP_033,Nb_002,Nb_006,Nb_007,Nb_015,Nb_019,Nb_025,Nb_031,Nb_033,Nb_038,Nb_062,Nb_095,GC129427,GC129428,\
-GC129429,GC129430,GC129431,GC129432,GC129433,GC129434,GC129435,GC129437,GC129438,GC136117,GC136119,GC136120,GC136121,GC136122,GC136123,GC136124,GC136125,GC136126,GC136127,GC136128 \
--Oz -o Pogonus_Np_Hst.vcf.gz /scratch/leuven/357/vsc35707/GWAS/merged_variants.vcf.gz
+bcftools view -S GC129388.primDud.filtered.sorted.nd.bam GC129389.primDud.filtered.sorted.nd.bam GC129390.primDud.filtered.sorted.nd.bam GC129391.primDud.filtered.sorted.nd.bam \
+GC129392.primDud.filtered.sorted.nd.bam GC129393.primDud.filtered.sorted.nd.bam Na_034.primDud.filtered.sorted.nd.bam PcNP_034.primDud.filtered.sorted.nd.bam \
+PcNP_037.primDud.filtered.sorted.nd.bam PcNP_040.primDud.filtered.sorted.nd.bam PcNP_044.primDud.filtered.sorted.nd.bam PcNP_045.primDud.filtered.sorted.nd.bam \
+PcNP_033.primDud.filtered.sorted.nd.bam Nb_002.primDud.filtered.sorted.nd.bam Nb_006.primDud.filtered.sorted.nd.bam Nb_007.primDud.filtered.sorted.nd.bam \
+Nb_015.primDud.filtered.sorted.nd.bam Nb_019.primDud.filtered.sorted.nd.bam Nb_025.primDud.filtered.sorted.nd.bam Nb_031.primDud.filtered.sorted.nd.bam \
+Nb_033.primDud.filtered.sorted.nd.bam Nb_038.primDud.filtered.sorted.nd.bam Nb_062.primDud.filtered.sorted.nd.bam Nb_095.primDud.filtered.sorted.nd.bam \
+GC129394.primDud.filtered.sorted.nd.bam GC129395.primDud.filtered.sorted.nd.bam GC129396.primDud.filtered.sorted.nd.bam GC129397.primDud.filtered.sorted.nd.bam \
+GC129398.primDud.filtered.sorted.nd.bam GC129399.primDud.filtered.sorted.nd.bam GC136084.primDud.filtered.sorted.nd.bam GC136085.primDud.filtered.sorted.nd.bam \
+GC136086.primDud.filtered.sorted.nd.bam GC136087.primDud.filtered.sorted.nd.bam GC136088.primDud.filtered.sorted.nd.bam GC136089.primDud.filtered.sorted.nd.bam \
+Db_101.primDud.filtered.sorted.nd.bam Db_102.primDud.filtered.sorted.nd.bam Db_103.primDud.filtered.sorted.nd.bam Pc_DZ_001.primDud.filtered.sorted.nd.bam \
+Pc_DZ_008.primDud.filtered.sorted.nd.bam Pc_DZ_012.primDud.filtered.sorted.nd.bam Pc_DZ_016.primDud.filtered.sorted.nd.bam Pc_DZ_018.primDud.filtered.sorted.nd.bam \
+Pc_DZ_022.primDud.filtered.sorted.nd.bam Pc_DZ_024.primDud.filtered.sorted.nd.bam Pc_DZ_027.primDud.filtered.sorted.nd.bam Pc_DZ_030.primDud.filtered.sorted.nd.bam \
+GC129427.primDud.filtered.sorted.nd.bam GC129428.primDud.filtered.sorted.nd.bam GC129429.primDud.filtered.sorted.nd.bam GC129430.primDud.filtered.sorted.nd.bam \
+GC129431.primDud.filtered.sorted.nd.bam GC129432.primDud.filtered.sorted.nd.bam GC129433.primDud.filtered.sorted.nd.bam GC129434.primDud.filtered.sorted.nd.bam \
+GC129435.primDud.filtered.sorted.nd.bam GC129437.primDud.filtered.sorted.nd.bam GC129438.primDud.filtered.sorted.nd.bam GC136117.primDud.filtered.sorted.nd.bam \
+GC136119.primDud.filtered.sorted.nd.bam GC136120.primDud.filtered.sorted.nd.bam GC136121.primDud.filtered.sorted.nd.bam GC136122.primDud.filtered.sorted.nd.bam \
+GC136123.primDud.filtered.sorted.nd.bam GC136124.primDud.filtered.sorted.nd.bam GC136125.primDud.filtered.sorted.nd.bam GC136126.primDud.filtered.sorted.nd.bam \
+GC136127.primDud.filtered.sorted.nd.bam GC136128.primDud.filtered.sorted.nd.bam \
+-Oz -o /scratch/leuven/357/vsc35707/GWAS/Belgium/Belgium.vcf.gz /scratch/leuven/357/vsc35707/GWAS/merged_variants.vcf.gz
 
-bcftools view -s GC129388,GC129389,GC129390,GC129391,GC129392,GC129393,Na_034,PcNP_034,PcNP_037,PcNP_040,PcNP_044,PcNP_045,PcNP_033,\
-Nb_002,Nb_006,Nb_007,Nb_015,Nb_019,Nb_025,Nb_031,Nb_033,Nb_038,Nb_062,Nb_095,\
-GC129394,GC129395,GC129396,GC129397,GC129398,GC129399,GC136084,GC136085,GC136086,GC136087,GC136088,GC136089,\
-Db_101,Db_102,Db_103,Pc_DZ_001,Pc_DZ_008,Pc_DZ_012,Pc_DZ_016,Pc_DZ_018,Pc_DZ_022,Pc_DZ_024,Pc_DZ_027,Pc_DZ_030,\
-GC129427,GC129428,GC129429,GC129430,GC129431,GC129432,GC129433,GC129434,GC129435,GC129437,GC129438,GC136117,GC136119,GC136120,GC136121,GC136122,GC136123,GC136124,GC136125,GC136126,GC136127,GC136128 \
--O z -o Pogonus_BE.vcf.gz merged_variants.vcf.gz
-
-
-bcftools query -l Pogonus_Np_Hst.vcf.gz
-bcftools index Pogonus_Np_Hst.vcf.gz
-
-bcftools query -l Pogonus_BE.vcf.gz
-bcftools index Pogonus_BE.vcf.gz
+bcftools query -l Belgium.vcf.gz
+bcftools index Belgium.vcf.gz
