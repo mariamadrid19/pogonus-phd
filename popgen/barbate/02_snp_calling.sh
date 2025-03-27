09451c0e1a473e35b13fe75ddd7cee168945db36
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-03-27 10:04:09 +0100

    Update 02_snp_calling.sh

--- popgen/barbate/02_snp_calling.sh
+++ popgen/barbate/02_snp_calling.sh
@@ -29,7 +29,7 @@ chrom=(CM008230.1_RagTag CM008231.1_RagTag CM008233.1_RagTag CM008234.1_RagTag C
 names=(1 10 2 3 4 5 6 7 8 9)
 
 # Sample IDs (20 samples)
-samples=(Bar2_01 Bar2_03 Bar2_04 Bar4_01 Bar4_04 Bar4_06)
+samples=(Bar2_01 Bar2_02 Bar2_03 Bar2_04 Bar2_05 Bar2_06 Bar2_07 Bar2_08 Bar2_09 Bar2_10 Bar4_01 Bar4_02 Bar4_03 Bar4_04 Bar4_05 Bar4_06 Bar4_07 Bar4_08 Bar4_09 Bar4_10)
 
 echo "${samples[ID]}"
 
