ddb9f4ff5b2eed4973717d5ffa8148fcaae40eab
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-04-15 17:14:22 +0200

    Update 001_filtering.sh

--- primary-assembly/001_filtering.sh
+++ primary-assembly/001_filtering.sh
@@ -9,6 +9,6 @@
 #SBATCH -o ont_filter.%j.out
 
 # filter ONT long reads based on a minimum length of 5kb and the best 90% (based on quality) of those reads
-filtlong --min_length 5000 --keep_percent 90 GC157812.fastq.gz | gzip >  GC157812_filtered.fastq
+filtlong --min_length 2000 --keep_percent 90 GC157812.fastq.gz | gzip >  GC157812_filtered.fastq
 
-filtlong --min_length 5000 --keep_percent 90 GC157813.fastq.gz | gzip >  GC157813_filtered.fastq
+filtlong --min_length 2000 --keep_percent 90 GC157813.fastq.gz | gzip >  GC157813_filtered.fastq
