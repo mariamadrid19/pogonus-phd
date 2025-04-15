102a5a48a2374bfdee1cc86fb04e9a1be90bacaf
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-04-15 15:58:35 +0200

    Update 003_kmercount.sh

--- primary-assembly/003_kmercount.sh
+++ primary-assembly/003_kmercount.sh
@@ -2,9 +2,8 @@
 #SBATCH --cluster=genius 
 #SBATCH --job-name=ont_filter
 #SBATCH --nodes=1 
-#SBATCH --ntasks-per-node=12
-#SBATCH --mem-per-cpu=32G 
-#SBATCH --time=12:00:00 
+#SBATCH --ntasks-per-node=32
+#SBATCH --time=24:00:00 
 #SBATCH -A lp_svbelleghem
 #SBATCH -o ont_filter.%j.out
 
@@ -12,8 +11,8 @@ seqtk seq -a GC157812_filtered.fastq > GC157812_filtered.fasta
 
 module load Jellyfish/2.2.10-intel-2018a
 
-#this will count kmers on the fasta file, takes a long time
-jellyfish count -m 21 -s 100M -t 32 -C GC157812_filtered.fasta
+#this will count kmers on the fasta file, takes a long time ( k = 17 is typical for long reads)
+jellyfish count -m 17 -s 100M -t 32 -C GC157812_filtered.fasta
 
 #this makes the k-mer histogram (can be seen on the .out file, DO NOT DELETE IT)
 jellyfish histo -t 32 mer_counts.jf > mer_counts.histo
