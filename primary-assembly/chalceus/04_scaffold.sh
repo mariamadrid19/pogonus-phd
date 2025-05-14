3d319e9416e3cc610097e97d0b1bfe535dce9f0e
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-05-14 15:17:23 +0200

    Update 04_scaffold.sh

--- primary-assembly/chalceus/04_scaffold.sh
+++ primary-assembly/chalceus/04_scaffold.sh
@@ -24,5 +24,10 @@ awk '/^>/ { if (count++ >= 20) exit } { print }'  old_ref.fa  > old_ref_20.fa
 # scaffold the contigs based on the old assembly (linkage groups)
 ragtag.py scaffold old_ref.fa P_chalceus_sorted.fa -t 32 -o P_chalceus_scaffolds
 
+samtools faidx ragtag.scaffold.fasta
+# Count how many of the scaffolds were placed as scaffolds based on the old reference genome (RagTag scaffolds)
+awk '$1 ~ /^CM00/ {sum += $2} END {print sum}' ragtag.scaffold.fasta.fai # a total of 303,620,366 bps (longer than the old reference)
+awk '$1 ~ /_RagTag$/ {sum += $2} END {print sum}' ragtag.scaffold.fasta.fai # 400,642,911 bps
+
 # Check completeness with minibusco
 compleasm.py run -a P_chalceus_scaffolds/ragtag.scaffold.fasta -o results_scaffolds/ -l coleoptera -t 72
