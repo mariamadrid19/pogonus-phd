1931ea9cda05a1863a8aa0fab86e98046f09db34
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-05-15 17:32:40 +0200

    Update 07-eviann.sh

--- annotation/07-eviann.sh
+++ annotation/07-eviann.sh
@@ -7,9 +7,15 @@
 #SBATCH -o eviann.%j.out
 #SBATCH -A lp_svbelleghem
 
+mamba activate entrez
+# use esearch and efetch to download all the proteins for insects available in NCBI
+esearch -db protein -query "txid50557[Organism:exp]" | efetch -format fasta > insecta_proteins.fasta
+
+# generate a txt file with all the names of the RNAseq reads needed (they should all be in a directory called rnaseq)
 cd rnaseq/
-paste <(ls $PWD/*_R1.fastq) <(ls $PWD/*_R2.fastq) > paired.txt
+paste <(ls $PWD/*_R1.fastq) <(ls $PWD/*_R2.fastq) > paired_mixed.txt
 
 cd ../
 
-eviann.sh -t 24 -g sorted_prim_dud.fasta -r paired.txt -p sequence.fasta -m 500000
+# run EviAnn with the proteins and the RNA sequences
+eviann.sh -t 24 -g sorted_prim_dud.fasta -r paired_mixed.txt -p insecta_proteins.fasta -m 500000
