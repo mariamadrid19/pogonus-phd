806a05728c5085b39b009e3a7926fbb37b753330
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-05-21 10:55:02 +0200

    Create 01_subset.sh

---
+++ primary-assembly/olivaceus/01_subset.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name Polv_sort
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=12:00:00
+#SBATCH -o Polv_sort.%j.out
+#SBATCH -A lp_svbelleghem
+
+conda activate thesis
+
+# Sort the HiFi reads in ascending order
+seqkit sort --by-length --reverse P_olivaceus_HiFi_reads.fasta > sorted_hifi.fasta
+
+# Take only 30% of the longest reads
+seqkit head -n 1800000 sorted_hifi.fasta -o P_olivaceus_HiFi_top30.fasta
