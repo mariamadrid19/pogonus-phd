7fcb75859c22cf0d0d57be1cd71c9b7bc4811658
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-05-26 20:42:39 +0200

    Update 05_scaffold.sh

--- primary-assembly/chalceus/05_scaffold.sh
+++ primary-assembly/chalceus/05_scaffold.sh
@@ -11,7 +11,10 @@ source /data/leuven/357/vsc35707/miniconda3/etc/profile.d/conda.sh
 conda activate thesis 
 
 #YaHs will take the contig sequences (.fa) and the HiC aligned to the contigs (.bam produced in step 4) and scaffold them 
-yahs purged.fa mapped.marked.bam
+yahs purged.fa mapped.marked.bam  -q 40 -l 10000 -R 2
+# q - Increase minimum mapping quality to 40. Removes noisy/ambiguous Hi-C contacts.
+# l - Scaffold only contigs > 10 kb. Smaller contigs tend to be repetitive or misassembled and can fragment scaffolds.
+# R - Run 2 rounds of scaffolding at each resolution level. May improve robustness without overfitting.
 
 #final scaffolds (.fa) are used to run the ARIMA pipeline again, the mapping will be using the scaffolds as the reference (instead of the contigs)
 mv yahs.out_scaffolds_final.fa P_chalceus_REF1.fa
