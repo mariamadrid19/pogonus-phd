c7fa47eb52c4293d3179618c369fe2d7f394f723
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-05-30 11:01:50 +0200

    Update 05b_REFscaffold.sh

--- primary-assembly/chalceus/05b_REFscaffold.sh
+++ primary-assembly/chalceus/05b_REFscaffold.sh
@@ -9,17 +9,22 @@
 
 conda activate thesis
 seqkit sort --by-length --reverse P_chalceus_REF1.fa  > P_chalceus_REF1_sorted.fa
+awk '/^>/ { if (count++ >= 11) exit } { print }' old_ref.fa > 11_old_ref.fa
 
 conda deactivate
 conda activate ragtag
 
 # scaffold the contigs based on the old assembly (linkage groups)
-ragtag.py scaffold old_ref.fa P_chalceus_REF1_sorted.fa -t 32 -o REF1_scaffolds
+ragtag.py scaffold -u 11_old_ref.fa P_chalceus_REF1_sorted.fa -t 32 -o REF1_scaffolds
 
-samtools faidx ragtag.scaffold.fasta
+seqkit sort --by-length --reverse REF1_scaffolds/ragtag.scaffold.fasta > REF1_scaffolds/ragtag.scaffold.sorted.fasta
+
+samtools faidx REF1_scaffolds/ragtag.scaffold.fasta
+samtools faidx REF1_scaffolds/ragtag.scaffold.sorted.fasta
+
 # Count how many of the scaffolds were placed as scaffolds based on the old reference genome (RagTag scaffolds)
-awk '$1 ~ /^CM00/ {sum += $2} END {print sum}' REF1_scaffolds/ragtag.scaffold.fasta.fai # 357,042,250 bp placed as the main chromosomes
-awk '$1 ~ /_RagTag$/ {sum += $2} END {print sum}' REF1_scaffolds/ragtag.scaffold.fasta.fai # 421,067,932 bp placed in total
+awk '$1 ~ /^CM00/ {sum += $2} END {print sum}' REF1_scaffolds/ragtag.scaffold.fasta.fai
+awk '$1 ~ /_RagTag$/ {sum += $2} END {print sum}' REF1_scaffolds/ragtag.scaffold.fasta.fai
 
 # Check completeness with minibusco
 compleasm.py run -a REF1_scaffolds/ragtag.scaffold.fasta -o results_scaffolds/ -l coleoptera -t 72
