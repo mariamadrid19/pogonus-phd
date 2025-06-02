65fde588e4e99d71e79a60f541573fe21b19677f
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-06-02 12:55:37 +0200

    Create 07b_juicer.sh

---
+++ primary-assembly/chalceus/07b_juicer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -l
+#SBATCH --cluster=wice
+#SBATCH --job-name=juicer
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=36
+#SBATCH --time=72:00:00
+#SBATCH -o juicer.%j.out
+#SBATCH -A lp_svbelleghem
+
+MAPPED='mapped2.PT.bam'
+AGP='yahs.out_scaffolds_final.agp'
+INDEX_CONTIGS='purged.fa.fai'
+
+juicer pre -a -o out_JBAT pre $MAPPED $AGP $INDEX_CONTIGS > out_JBAT.log 2 > &1
+
+(java -Xmx48000m  -Djava.awt.headless=true -jar juicer_tools.2.20.00.jar pre out_JBAT.txt out_JBAT.hic.part <(cat out_JBAT.log  | grep PRE_C_SIZE | awk '{print $2" "$3}')) && (mv out_JBAT.hic.part out_JBAT.hic)
