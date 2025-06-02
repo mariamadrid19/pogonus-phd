24057205444ba00ac63852d274b339478f5504bf
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-06-02 16:45:20 +0200

    Update 02_snp_calling.sh

--- popgen/Coto/02_snp_calling.sh
+++ popgen/Coto/02_snp_calling.sh
@@ -20,42 +20,42 @@ source /data/leuven/357/vsc35707/miniconda3/etc/profile.d/conda.sh
 conda activate vcftools
 
 # Define reference
-REFNAME=P_chalceus_REF1
-REF=/scratch/leuven/357/vsc35707/popgen/${REFNAME}.fa
-FAI=${REF}.fai
+REFNAME="P_chalceus_REF1"
+REF="/scratch/leuven/357/vsc35707/popgen/${REFNAME}.fa"
+FAI="${REF}.fai"
 
-# Read scaffold names from the .fai file into an array
-readarray -t scaffolds < <(cut -f1 $FAI)
-
-# Get current scaffold
-CHR=${scaffolds[$ID]}
+# Get scaffold name
+readarray -t scaffolds < <(cut -f1 "$FAI")
+CHRNAME="${scaffolds[$ID]}"
 
 # Sample IDs
 samples=(GC136107 GC136108 GC136109 GC136110 GC136111 GC136112 GC136113 GC136114 GC136115 GC136116)
 
-# Generate list of BAM files
+# Build BAM file list
 ALL_LIST=""
 for SAMPLE in "${samples[@]}"; do
     ALL_LIST+=" $SAMPLE.${REFNAME}.filtered.sorted.nd.bam"
 done
 
-# Output file stem
-OUTBASE="Pogonus_${REFNAME}.${CHR}"
+# Output VCF stem
+VCF_CALL="Pogonus_${REFNAME}.${CHRNAME}"
 
-# Check validity
-if [[ -z "$CHR" ]]; then
-    echo "Error: scaffold ID is empty!"
+# Safety check
+if [[ -z "$CHRNAME" ]]; then
+    echo "Error: empty scaffold name at index $ID"
     exit 1
 fi
 
-# Variant calling
-bcftools mpileup -Oz --threads 20 -f "$REF" $ALL_LIST -r "$CHR" | bcftools call -m -Oz -o ${OUTBASE}.vcf.gz
+# Call variants with bcftools mpileup
+bcftools mpileup -Oz --threads 20 -f "$REF" $ALL_LIST -r "$CHRNAME" \
+    | bcftools call -m -Oz -o "${VCF_CALL}.vcf.gz"
 
-# Filtering with vcftools
-vcftools --gzvcf "${VCF_CALL}.vcf.gz" --recode --remove-indels --minQ 30 --max-missing 0.25 --stdout | bgzip > "${VCF_CALL}.filt.bi.vcf.gz"
+# Filter with vcftools
+vcftools --gzvcf "${VCF_CALL}.vcf.gz" --recode --remove-indels --minQ 30 --max-missing 0.25 --stdout \
+    | bgzip > "${VCF_CALL}.filt.bi.vcf.gz"
 
-# Parsing VCF with filtering rules
-/data/leuven/357/vsc35707/miniconda3/bin/python3.11 parseVCF.py \
+# Parse VCF with custom script
+python parseVCF.py \
   --gtf flag=GQ   min=30   gtTypes=Het \
   --gtf flag=GQ   min=30   gtTypes=HomAlt \
   --gtf flag=DP   min=10 \
@@ -63,8 +63,10 @@ vcftools --gzvcf "${VCF_CALL}.vcf.gz" --recode --remove-indels --minQ 30 --max-m
   -i "${VCF_CALL}.filt.bi.vcf.gz" \
   | gzip > "${VCF_CALL}.calls.filt.bi.vcf.gz"
 
-# Striping BAM suffix from SNP IDs
+# Strip BAM suffix from SNP IDs
 CALLS_H="Pogonus_${REFNAME}_chr_${CHRNAME}.H.calls"
-zcat "${VCF_CALL}.calls.filt.bi.vcf.gz" | sed 's/\.filtered\.sorted\.nd\.bam//g' | bgzip -c > "${CALLS_H}.gz"
+zcat "${VCF_CALL}.calls.filt.bi.vcf.gz" \
+    | sed 's/\.filtered\.sorted\.nd\.bam//g' \
+    | bgzip -c > "${CALLS_H}.gz"
 
 echo "Done chr ${CHRNAME}."
