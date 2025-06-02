351f475cf789319ca8e98a00132f12dfaf50111d
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-06-02 15:23:04 +0200

    Update 02_snp_calling.sh

--- popgen/Coto/02_snp_calling.sh
+++ popgen/Coto/02_snp_calling.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l 
 #SBATCH --cluster=genius 
-#SBATCH --job-name barbate_snps
+#SBATCH --job-name spain_snps
 #SBATCH --nodes=1 
-#SBATCH --ntasks-per-node=20 
+#SBATCH --cpus-per-task=36
 #SBATCH --time=72:00:00 
 #SBATCH -A lp_svbelleghem
-#SBATCH -o barbate_snps.%j.out
+#SBATCH -o spain_snps.%j.out
 #SBATCH --array=1-150
 
 # This variable will store the job array number minus 1, so we can use it to get a sample from the samples list (index starts at 0)
@@ -26,11 +26,11 @@ echo "================="
 chrom=(CM008230.1_RagTag CM008231.1_RagTag CM008233.1_RagTag CM008234.1_RagTag CM008235.1_RagTag CM008236.1_RagTag CM008237.1_RagTag CM008238.1_RagTag CM008239.1_RagTag CM008240.1_RagTag)
 names=(1 10 2 3 4 5 6 7 8 9)
 
-# Sample IDs (20 samples)
-samples=(Bar2_01 Bar2_02 Bar2_03 Bar2_04 Bar2_05 Bar2_06 Bar2_07 Bar2_08 Bar2_09 Bar2_10 Bar4_01 Bar4_02 Bar4_03 Bar4_04 Bar4_05 Bar4_06 Bar4_07 Bar4_08 Bar4_09 Bar4_10)
+# Sample IDs (10 samples)
+samples=(GC136107 GC136108 GC136109 GC136110 GC136111 GC136112 GC136113 GC136114 GC136115 GC136116)
 
-REFNAME=dudPrim
-REF=/scratch/leuven/357/vsc35707/BAR_mapping/sorted_prim_dud.fasta
+REFNAME=P_chalceus_REF1
+REF=/scratch/leuven/357/vsc35707/popgen/P_chalceus_REF1.fa
 
 echo "${samples[ID]}"
 
@@ -59,17 +59,17 @@ if [[ -z "$command" ]]; then
 fi
 
 # run mpileup
-bcftools mpileup -Oz --threads 20 -f $REF $(echo $command) -r $(echo "${chrom[ID]}") | bcftools call -m -Oz -o Pogonus_Barbate_$REFNAME.chr_$(echo "${names[ID]}").vcf.gz
+bcftools mpileup -Oz --threads 20 -f $REF $(echo $command) -r $(echo "${chrom[ID]}") | bcftools call -m -Oz -o Pogonus_$REFNAME.chr_$(echo "${names[ID]}").vcf.gz
 
-vcftools --gzvcf Pogonus_Barbate_$REFNAME.chr_$(echo "${names[ID]}").vcf.gz --recode --remove-indels --minQ 30 --max-missing 0.25 --stdout | bgzip > Pogonus_Barbate_$REFNAME.chr_$(echo "${names[ID]}").filtered.vcf.gz
+vcftools --gzvcf Pogonus_$REFNAME.chr_$(echo "${names[ID]}").vcf.gz --recode --remove-indels --minQ 30 --max-missing 0.25 --stdout | bgzip > Pogonus_$REFNAME.chr_$(echo "${names[ID]}").filtered.vcf.gz
 
-bgzip Pogonus_Barbate_$REFNAME.chr_$(echo "${names[ID]}").filtered.vcf.gz
+bgzip Pogonus_$REFNAME.chr_$(echo "${names[ID]}").filtered.vcf.gz
 
-#python parseVCF.py --minQual 30 --skipIndels -i Pogonus_Barbate_$REFNAME.chr_$(echo "${names[ID]}").filtered.vcf.gz  | gzip > Pogonus_Barbate_$REFNAME.chr_$(echo "${names[ID]}").calls.gz
+#python parseVCF.py --minQual 30 --skipIndels -i Pogonus_$REFNAME.chr_$(echo "${names[ID]}").filtered.vcf.gz  | gzip > Pogonus_$REFNAME.chr_$(echo "${names[ID]}").calls.gz
 
-python parseVCF.py --gtf flag=GQ min=30 gtTypes=Het --gtf flag=GQ min=30 gtTypes=HomAlt --gtf flag=DP min=10 --skipIndels -i Pogonus_Barbate_$REFNAME.chr_$(echo "${names[ID]}").filtered.vcf.gz | gzip > Pogonus_Barbate_$REFNAME.chr_$(echo "${names[ID]}").calls.gz
+python parseVCF.py --gtf flag=GQ min=30 gtTypes=Het --gtf flag=GQ min=30 gtTypes=HomAlt --gtf flag=DP min=10 --skipIndels -i Pogonus_$REFNAME.chr_$(echo "${names[ID]}").filtered.vcf.gz | gzip > Pogonus_$REFNAME.chr_$(echo "${names[ID]}").calls.gz
 #--gtf flag=GQ min=30 gtTypes=Het > Applies a filter (>30) on the genotype quality field only to heterozygous genotypes (e.g., 0/1).
 #--gtf flag=GQ min=30 gtTypes=HomAlt > Same as above, but for homozygous alternate genotypes (e.g., 1/1).
 #--gtf flag=DP min=10 > Filters based on DP (read depth), keeps genotypes only if DP ≥ 10, regardless of genotype type.
 
-zcat Pogonus_Barbate_$REFNAME.chr_$(echo "${names[ID]}").calls.gz | sed 's/.dudPrim.filtered.sorted.nd.bam//g' | bgzip > Pogonus_Barbate_$REFNAME.chr_$(echo "${names[ID]}").H.calls.gz
+zcat Pogonus_$REFNAME.chr_$(echo "${names[ID]}").calls.gz | sed 's/.dudPrim.filtered.sorted.nd.bam//g' | bgzip > Pogonus_$REFNAME.chr_$(echo "${names[ID]}").H.calls.gz
