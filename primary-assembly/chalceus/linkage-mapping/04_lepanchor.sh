324fb17b7ae59741efc40610083dc34819c2bfe5
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-06-09 01:13:40 +0200

    Update 04_lepanchor.sh

--- primary-assembly/chalceus/linkage-mapping/04_lepanchor.sh
+++ primary-assembly/chalceus/linkage-mapping/04_lepanchor.sh
@@ -14,6 +14,38 @@ cd /scratch/leuven/357/vsc35707/linkage-mapping/lepmap
 
 # Define directories
 LEPANCHOR="/data/leuven/357/vsc35707/LepAnchor"
+LEPMAP="/data/leuven/357/vsc35707/LepMap3"
+SCAFFOLDS="/scratch/leuven/357/vsc35707/linkage-mapping/genome/P_chalceus_broken.f"
 
 # Clean map file
-java -cp bin/ CleanMap map=map.txt >map.clean
+java -cp $LEPANCHOR/bin/ CleanMap map=cleanMap.input > map.clean
+
+# Filter and process map.clean
+awk '($5>=1.0)' map.clean | awk -f $LEPANCHOR/cleanmap.awk | awk -f $LEPANCHOR/joinIntervals.awk P_chalceus_broken.fa.sizes - > map.bed
+
+# Create .bed files for each chromosome 
+awk '{fn="chr" $4 ".bed"; print $1"\t"$2"\t"$3 > fn}' map.bed
+
+# Run OrderMarkers2 on chromosomes 1 to 11
+for X in {1..11}; do
+  java -cp $LEPMAP/bin OrderMarkers2 map=map5.txt data=data_f.call chromosome=$X recombination1=0 > order${X}.txt
+done
+
+# Prepare input for PlaceAndOrientContigs
+for x in {1..11}; do
+  awk -vn=$x '(NR==FNR){map[NR-1]=$0} (NR!=FNR && /^[^#]/){print map[$1],n,$2,$3}' snps.txt order$x.txt > order$x.m.input
+done
+
+# Run PlaceAndOrientContigs
+for X in {1..11}; do
+  java -cp $LEPANCHOR/bin/ PlaceAndOrientContigs map=order$X.m.input bed=chr$X.bed noIntervals=1 > chr$X.la 2> chr$X.la.err
+done
+
+# Generate .agp files
+for f in chr*.la; do
+  chrnum=$(echo $f | sed 's/[^0-9]//g')
+  awk -vlg=$chrnum -f $LEPANCHOR/makeagp_full2.awk $f > chr$chrnum.agp
+done
+
+# Create the final fasta file
+awk -f $LEPANCHOR/makefasta.awk $SCAFFOLDS chr*.agp > final.fasta
