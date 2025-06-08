0973d11042613350687b91a1d545a80d0ee566d3
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-06-09 01:05:58 +0200

    Update 04_lepanchor.sh

--- primary-assembly/chalceus/linkage-mapping/04_lepanchor.sh
+++ primary-assembly/chalceus/linkage-mapping/04_lepanchor.sh
@@ -9,4 +9,11 @@
 #SBATCH -A lp_svbelleghem
 #SBATCH -o lepanchor.%j.out
 
+# Change to the working directory
 cd /scratch/leuven/357/vsc35707/linkage-mapping/lepmap
+
+# Define directories
+LEPANCHOR="/data/leuven/357/vsc35707/LepAnchor"
+
+# Clean map file
+java -cp bin/ CleanMap map=map.txt >map.clean
