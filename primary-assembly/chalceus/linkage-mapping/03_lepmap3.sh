36677cc808ca6f55545c59c9756957fef4fc3005
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-06-09 01:04:34 +0200

    Update 03_lepmap3.sh

--- primary-assembly/chalceus/linkage-mapping/03_lepmap3.sh
+++ primary-assembly/chalceus/linkage-mapping/03_lepmap3.sh
@@ -16,17 +16,17 @@ cd /scratch/leuven/357/vsc35707/linkage-mapping/lepmap
 LEPMAP="/data/leuven/357/vsc35707/LepMap3"
 
 # Run ParentCall2
-zcat post.gz|java -cp $LEPMAP/bin ParentCall2 data=pedigree.txt posteriorFile=- removeNonInformative=1|gzip >data.call.gz
-zcat data.call.gz | awk 'NR>=7' | cut -f 1,2 > snps.txt
+zcat post.gz|java -cp $LEPMAP/bin ParentCall2 data=pedigree.txt posteriorFile=- removeNonInformative=1 | gzip > data.call.gz
 
 # Run Filtering2
-zcat data.call.gz|java -cp $LEPMAP/bin Filtering2 data=- dataTolerance=0.01|gzip >data_f_t01.call.gz
+zcat data.call.gz | java -cp $LEPMAP/bin Filtering2 data=- dataTolerance=0.01 | gzip > data_f_t01.call.gz
+zcat data_f_t01.call.gz | awk 'NR>=7' | cut -f 1,2 > snps.txt
 
 # Run SeparateChromosomes2 
-zcat data_f_t01.call.gz | java -cp $LEPMAP/bin SeparateChromosomes2 data=- lodLimit=5 >map5.txt
+zcat data_f_t01.call.gz | java -cp $LEPMAP/bin SeparateChromosomes2 data=- lodLimit=5 > map5.txt
 
 # CleanMap (for LepAnchor)
-paste snps.txt map5.txt|awk '(NR>1)' >cleanMap.input
+paste snps.txt map5.txt|awk '(NR>1)' > cleanMap.input
 
-# Run OrderMakers2 (per chromosome, as assigned by 
+# Run OrderMakers2 (per chromosome, as assigned by SeparateChromosomes2)
 java -cp /data/leuven/357/vsc35707/LepMap3/bin/ OrderMarkers2 map=map5.txt data=data_f.call chromosome=1 recombination1=0 > order1.txt
