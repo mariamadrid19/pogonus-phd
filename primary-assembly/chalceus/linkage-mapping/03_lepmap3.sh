1011d7f741107939386449338d4ae713f7eaa923
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-06-10 13:29:54 +0200

    Update 03_lepmap3.sh

--- primary-assembly/chalceus/linkage-mapping/03_lepmap3.sh
+++ primary-assembly/chalceus/linkage-mapping/03_lepmap3.sh
@@ -23,7 +23,10 @@ zcat data.call.gz | java -cp $LEPMAP/bin Filtering2 data=- dataTolerance=0.01 | 
 zcat data_f_t01.call.gz | awk 'NR>=7' | cut -f 1,2 > snps.txt
 
 # Run SeparateChromosomes2 
-zcat data_f_t01.call.gz | java -cp $LEPMAP/bin SeparateChromosomes2 data=- lodLimit=5 > map5.txt
+zcat data_f_t01.call.gz | java -cp $LEPMAP/bin SeparateChromosomes2 data=- lodLimit=6 > map6.txt
+
+# Run JoinSingles2All
+zcat data_f_t01.call.gz | java -cp $LEPMAP/bin JoinSingles2All map=map6.txt data=- lodLimit=5 iterate=2 >map6js.txt
 
 # Create the input for CleanMap (for LepAnchor)
-paste snps.txt map5.txt|awk '(NR>1)' > cleanMap.input
+paste snps.txt map6js.txt|awk '(NR>1)' > cleanMap.input
