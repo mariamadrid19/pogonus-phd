2c5d321570bb4cae444907a3e973bee8e0a81d1a
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2025-07-24 14:13:35 +0200

    Update 03_fst.sh

--- popgen/new-genome/03_fst.sh
+++ popgen/new-genome/03_fst.sh
@@ -1,36 +1,35 @@
 #!/bin/bash -l
 #SBATCH --cluster=genius
 #SBATCH --job-name=fst
-#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=20
 #SBATCH --time=12:00:00
 #SBATCH -A lp_svbelleghem
 #SBATCH -o fst.%A_%a.out
-#SBATCH --array=0-19
+#SBATCH --array=0-54
 
 # Array ID breakdown
-CHR_INDEX=$((SLURM_ARRAY_TASK_ID / 2 + 1))  # Chromosome 1–10
-PAIR_INDEX=$((SLURM_ARRAY_TASK_ID % 2))     # 0 or 1 for pair 1 or 2
+CHR_INDEX=$((SLURM_ARRAY_TASK_ID / 5 + 1))   # Chromosome 1 to 11
+PAIR_INDEX=$((SLURM_ARRAY_TASK_ID % 5))      # Comparison 0 to 4
 
 echo "==================="
 echo "Processing CHR${CHR_INDEX}, POPULATION COMPARISON ${PAIR_INDEX}"
 
 # Define population pairs
-pop1=("B_NIE_T" "S_HUE_T")
-pop2=("B_DUD_S" "S_COT_S")
-POPSFILE=popfile.txt
+pop1=("B_NIE_T" "F_GUE_T" "P_AVE_T" "S_HUE_T" "E_SEV_T")
+pop2=("B_DUD_S" "F_GUE_S" "P_AVE_S" "S_COT_S" "F_CAM_S")
+POPSFILE=popfileALL.txt
 
 # Paths
 CALLS_DIR="final-calls"
 OUTDIR="final-stats"
 mkdir -p "$OUTDIR"
 
-CALLS_FILE="${CALLS_DIR}/Pchal.chr_${CHR_INDEX}.H.calls.gz"
-OUTFILE="${OUTDIR}/Pchal.chr_${CHR_INDEX}.pop${PAIR_INDEX}.stats"
+CALLS_FILE="${CALLS_DIR}/Pchal_Bar_SW.chr_${CHR_INDEX}.H.calls.gz"
+OUTFILE="${OUTDIR}/Pchal_Bar_SW.chr_${CHR_INDEX}.pop${PAIR_INDEX}.stats"
 
 # Run popgen analysis
 /data/leuven/357/vsc35707/miniconda3/bin/python popgenWindows_egglib.py \
-    -w 25000 -s 12500 --minSites 250 --maxMissing 0.5 --calcMissing \
+    -w 25000 -s 12500 --minSites 250 --maxMissing 0.25 \
     -T 10 --windType coordinate -f phased \
     -g "$CALLS_FILE" \
     --popsFile "$POPSFILE" \
