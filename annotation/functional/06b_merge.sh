c79b67d89bede477a59cf43b860b7ce211234870
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2026-03-30 15:38:22 +0200

    Create 06b_merge.sh

---
+++ annotation/functional/06b_merge.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -l
+#SBATCH --cluster=genius
+#SBATCH --job-name merge_annotations
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=12
+#SBATCH --time=2:00:00
+#SBATCH -o /scratch/leuven/357/vsc35707/annotation/func-annotation/logs/merge.%j.out
+#SBATCH --error=/scratch/leuven/357/vsc35707/annotation/func-annotation/logs/merge.%j.err
+#SBATCH -A lp_svbelleghem
+
+module load Python/3.13.1-GCCcore-14.2.0
+
+python3 /scratch/leuven/357/vsc35707/annotation/func-annotation/scripts/merge_annotations.py
