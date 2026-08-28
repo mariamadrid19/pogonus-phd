f7a4c9785e1513c4a317dc81a9e8777729e00ed7
Maria A. Madrid-Restrepo <75859991+mariamadrid19@users.noreply.github.com>
2026-08-28 13:47:20 +0200

    Update SLURM job parameters in merge_annotations script

--- annotation/functional/06b_merge.sh
+++ annotation/functional/06b_merge.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -l
 #SBATCH --cluster=wice
-#SBATCH --job-name merge_annotations
+#SBATCH --job-name=merge_annotations
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=12
-#SBATCH --time=2:00:00
-#SBATCH -o /scratch/leuven/357/vsc35707/annotation/func-annotation/logs/merge.%j.out
+#SBATCH --cpus-per-task=1
+#SBATCH --time=02:00:00
+#SBATCH --output=/scratch/leuven/357/vsc35707/annotation/func-annotation/logs/merge.%j.out
 #SBATCH --error=/scratch/leuven/357/vsc35707/annotation/func-annotation/logs/merge.%j.err
-#SBATCH -A lp_svbelleghem
+#SBATCH --account=lp_svbelleghem
+
+set -euo pipefail
 
 module load Python/3.13.1-GCCcore-14.2.0
 
-python3 /scratch/leuven/357/vsc35707/annotation/func-annotation/scripts/merge_annotations.py
+python3 /scratch/leuven/357/vsc35707/annotation/func-annotation/scripts/merge_annotations.py \
+    /scratch/leuven/357/vsc35707/annotation/func-annotation
